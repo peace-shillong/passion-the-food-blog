f9f08a2836001d44ff8019e373f564dba054617d
KYRSHANLANG R DKHAR <33800152+peace-shillong@users.noreply.github.com>
2017-12-02 01:03:26 +0530

    Index page Template with dummy data

---
+++ index.php
@@ -0,0 +1,48 @@
+	<?php include("includes/header.php")?>
+		<!--IMG Slider-->
+	<link href="dist/css/responsiveslides.css" rel="stylesheet" type="text/css" />	
+	<!-- required -->
+	<div class="content">
+	<p>
+	<ul class="rslides" style="z-index:-9999;">
+      <li><img src="dist/images/slide-1.jpg"/> </li>         
+      <li> <img src="dist/images/slide-2.jpg" /> </li> 
+      <li> <img src="dist/images/slide-3.jpg"/> </li>        
+	  <li> <img src="dist/images/slide-4.jpg"/> </li>        
+	  <li> <img src="dist/images/slide-5.jpg"/> </li>     
+	  <li> <img src="dist/images/slide-6.jpg"/> </li>        	  
+    </ul>
+	<br/>	
+	</p>
+	<p>
+	Test 1 
+	<br/><br/><br/><br/><br/><br/>
+	</p>
+	<p>
+	Test 2 
+	<br/><br/><br/><br/><br/><br/>
+	</p>
+	<p><br/><br/><br/><br/><br/>
+	Test 3 
+	<br/><br/><br/><br/>
+	</p>
+	<p>
+	Test 4 
+	<br/><br/><br/><br/><br/><br/>
+	</p>
+	<p>
+	Test 5
+	<br/><br/><br/><br/><br/><br/>
+	</p>
+	<p>
+	Test 6
+	<br/><br/><br/><br/><br/><br/>
+	</p>
+	<p>
+	Test
+	<br/><br/><br/><br/><br/><br/>
+	</p>
+	</div>
+	
+	<?php include("includes/footer.php")?>
+	
