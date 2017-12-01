6ddecd483be9fdf306d2f49a096fa6285c4d3c00
KYRSHANLANG R DKHAR <33800152+peace-shillong@users.noreply.github.com>
2017-12-02 01:08:07 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+	
+	<div class="header" >
+		 <a href="#" style="font-size:24px;color:#80020a;text-align:left;margin-left:15%;"><br/>&nbsp PASSION, GRILLS AND BBQ</a>
+	</div>
+	<div class="footer" >
+		 <span style="margin-left:15%;">&copy <?php echo date('Y');?> by Passion, Grills and BBQ</span>
+	</div>
+	<script type='text/javascript' src="dist/js/jquery.min.js"></script>
+	<script type="text/javascript" src="dist/js/mystellarnav.min.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			jQuery('.stellarnav').stellarNav({
+				theme: 'light'
+				
+			});
+		});
+	</script>
+	
+	<!--IMG-->
+	<script src="dist/js/responsiveslides.min.js"></script>
+	<script>
+	$(function() {
+		$(".rslides").responsiveSlides();
+	});
+	</script>
+</body>
+</html>
